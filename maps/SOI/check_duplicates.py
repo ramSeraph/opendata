d20ef50432b1f1a825aa71367cf83fb497b73e0a
ramSeraph <kandimalla.sreeram@gmail.com>
2022-05-27 01:54:19 +0530

    SOI: add check duplicates to shortcircuit in the presence of potential problems

---
+++ maps/SOI/check_duplicates.py
@@ -0,0 +1,29 @@
+
+import sys
+from pathlib import Path
+from filecmp import cmp
+from known_problems import known_problems
+
+file_list = Path(sys.argv[1]).read_text().split('\n')
+file_list = [ f.strip() for f in file_list if f.strip() != '' ]
+
+file_map = {}
+for filename in file_list:
+    if filename in known_problems:
+        continue
+    is_a_copy = False
+    for e_filename in file_map.keys():
+        if cmp(e_filename, filename):
+            file_map[e_filename].append(filename)
+            is_a_copy = True
+            break
+
+    if is_a_copy:
+        continue
+    file_map[filename] = []
+
+duplicate_map = {k:v for k,v in file_map.items() if len(v) != 0}
+print(duplicate_map)
+if len(duplicate_map) > 0:
+    exit(1)
+exit(0)
---
+++ maps/SOI/known_problems.py
@@ -0,0 +1,41 @@
+
+known_problems = [
+    #'data/raw/86K_7.pdf', # andaman, combined file
+    # TODO: some of the andaman sheets might still be in everest datum, check if that is the case
+    # also, check if the index corrections messed with the georeferencing
+
+    'data/raw/74B_4.pdf', # srikakulam, combined file handled in 74B_3
+    'data/raw/74B_7.pdf', # srikakulam, combined file handled in 74B_3
+
+    'data/raw/58C_1.pdf', # Kochi, combined file handled in 58C_5
+
+    'data/raw/65K_12.pdf', # east godavari, combined file handled in 65K_8
+
+    'data/raw/74B_10.pdf', # srikakulam, combined file handled in 74B_6
+
+    'data/raw/65O_3.pdf', # vishakapatnam, combined file handled in 65O_2
+
+    'data/raw/66D_5.pdf', # chennai, combined file handled in 66D_1
+
+    'data/raw/41B_16.pdf', # gujarat, combined files handled in 41F_4
+
+    'data/raw/41G_16.pdf', # gujarat, combined files handled in 41K_4
+
+    'data/raw/48E_5.pdf', # gujarat, combined files handled in 48E_9
+
+    'data/raw/41P_2.pdf', # una gujarat, combined files handled in 41P_1
+
+    'data/raw/41F_10.pdf', # gujarat, combined files handled in 41F_9
+    'data/raw/41F_6.pdf', # gujarat, combined files handled in 41F_7
+    'data/raw/49M_6.pdf', # kerala, combined files handled in 49M_10
+    'data/raw/48L_15.pdf', # kerala, combined files handled in 48P_3
+
+    # kept in for now
+    #'data/raw/48J_10.pdf', # anamoly, black strip in file
+    #'data/raw/49N_14.pdf', # anamoly, black strip in file
+
+    'data/raw/54N_12.pdf', # bad file
+    'data/raw/45D_14.pdf', # bad file
+    'data/raw/40M_11.pdf', # bad file
+]
+

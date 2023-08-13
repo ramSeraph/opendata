bcda270afbb3e4ce63de021d31c3abacf5da7594
ramSeraph <kandimalla.sreeram@gmail.com>
2023-08-13 23:03:36 +0530

    SOI: replace create release action with bash code

--- .github/actions/releaser/rfuncs.sh
+++ .github/actions/releaser/rfuncs.sh
@@ -23,6 +23,32 @@ function has_release {
     fi
 }
 
+function create_release {
+    record_call "$@"
+    tname="$1"
+    name="$2"
+    body="$3"
+    gh api --method POST -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/${GITHUB_REPOSITORY}/releases -f tag_name="${tname}" -f name="${name}" -f body="${body}" 2>>$err_file | jq .id
+}
+
+function upload_asset {
+    record_call "$@"
+    tag_name="$1"
+    fname="$2"
+    echo "uploading asset $fname" >>$err_file
+    gh release upload $tag_name $fname >>$err_file 2>>$err_file
+}
+
+function create_release_with_assets {
+    record_call "$@"
+    tname="$1"
+    name="$2"
+    body="$3"
+    files="$4"
+    create_release "$tname" "$name" "$body"
+    echo "$files" | while IFS= read -r file; do upload_asset "$tname" "$file"; done
+}
+
 function move_release {
     record_call "$@"
     from_id=$1

09613f62f79c818ea4294caeece0acb650bcd6e4
ramSeraph <kandimalla.sreeram@gmail.com>
2024-07-13 13:41:04 +0530

    SOI: fix retile script.. note to self: don't code when you are high

--- maps/SOI/retile.py
+++ maps/SOI/retile.py
@@ -11,6 +11,8 @@ from pathlib import Path
 from multiprocessing import Pool
 from functools import partial
 
+from pprint import pprint
+
 import mercantile
 
 from osgeo_utils.gdal2tiles import main as gdal2tiles_main
@@ -25,7 +27,6 @@ from google.cloud.storage.constants import _DEFAULT_TIMEOUT
 
 from tile_sources import DiskSource, PartitionedPMTilesSource, MissingTileError
 
-MAX_Z = int(os.environ.get('MAX_Z', '14'))
 SHEETS_FROM_GCS = os.environ.get("SHEETS_FROM_GCS", '1') == '1'
 TILES_FROM_GCS = os.environ.get("TILES_FROM_GCS", '0') == '1'
 TILES_TO_GCS = os.environ.get("TILES_TO_GCS", '0') == '1'
@@ -39,8 +40,6 @@ orig_tiles_dir = Path('export/tiles')
 orig_tiffs_dir = Path('export/gtiffs')
 tiles_dir = Path('staging/tiles')
 tiffs_dir = Path('staging/gtiffs')
-z_min = 2
-z_max = MAX_Z
 
 class AttrDict(dict):
     def __init__(self, *args, **kwargs):
@@ -59,6 +58,7 @@ def run_external(cmd):
     if res.returncode != 0:
         raise Exception(f'command {cmd} failed')
 
+
 def convert_paths_in_vrt(vrt_fname):
     # <SourceFilename relativeToVRT="1">40M_15.tif</SourceFilename>
     vrt_file = Path(vrt_fname)
@@ -71,25 +71,8 @@ def convert_paths_in_vrt(vrt_fname):
     )
     vrt_file.write_text(replaced)
 
-def get_sheets_to_basetile_mapping(index_data):
-    sheets_to_base_tiles = {}
-    base_tiles_to_sheets = {}
-    for f in index_data['features']:
-        sheet_no = f['properties']['EVEREST_SH'].replace('/', '_')
-        box = f['geometry']['coordinates'][0][:-1]
-        tiles = set(mercantile.tiles(box[0][0], box[2][1], box[2][0], box[0][1], [z_max])) 
-        sheets_to_base_tiles[sheet_no] = tiles
-        for tile in tiles:
-            if tile not in base_tiles_to_sheets:
-                base_tiles_to_sheets[tile] = set()
-            base_tiles_to_sheets[tile].add(sheet_no)
-    data = { 'base_tiles_to_sheets': base_tiles_to_sheets,
-             'sheets_to_base_tiles': sheets_to_base_tiles }
-    return data
-
-
     
-def create_tiles(inp_file, output_dir, zoom_levels):
+def create_base_tiles(inp_file, output_dir, zoom_levels):
     print('start tiling')
     os.environ['GDAL_CACHEMAX'] = '2048'
     os.environ['GDAL_MAX_DATASET_POOL_SIZE'] = '5000'
@@ -168,7 +151,6 @@ def get_tile_file_orig(tile):
     return f'{orig_tiles_dir}/{tile.z}/{tile.x}/{tile.y}.webp'
 
 def copy_sheets_over(sheets_to_pull):
-    print(sheets_to_pull)
     for sheet_no in sheets_to_pull:
         to = tiffs_dir.joinpath(f'{sheet_no}.tif')
         fro = orig_tiffs_dir.joinpath(f'{sheet_no}.tif')
@@ -209,7 +191,7 @@ def push_tiles(tiles_to_push):
         Path('to_gcs/all_done').write_text('')
 
 
-def create_upper_tiles(all_affected_tiles_by_zoom):
+def create_upper_tiles(tiles_to_create):
     options = AttrDict({
         'resume': True,
         'verbose': True,
@@ -229,117 +211,143 @@ def create_upper_tiles(all_affected_tiles_by_zoom):
         tile_extension='webp',
         kml=False
     )
-    nb_processes = 8
-    #with Pool(processes=nb_processes) as pool:
-    for z in range(z_max - 1, z_min - 1, -1):
-        print(f'creating overviews at level {z}')
-        for tile in all_affected_tiles_by_zoom[z]:
-            ctiles = mercantile.children(tile)
-            tiles_dir.joinpath(str(tile.z), str(tile.x)).mkdir(parents=True, exist_ok=True)
-            base_tile_group = [ (t.x, GDAL2Tiles.getYTile(t.y, t.z, options)) for t in ctiles ]
-            print(f'{tile=}, {base_tile_group=}')
-            create_overview_tile(z + 1, output_folder=str(tiles_dir), tile_job_info=tile_job_info, options=options, base_tiles=base_tile_group)
+
+    for tile in tiles_to_create:
+        ctiles = mercantile.children(tile)
+        tiles_dir.joinpath(str(tile.z), str(tile.x)).mkdir(parents=True, exist_ok=True)
+        base_tile_group = [ (t.x, GDAL2Tiles.getYTile(t.y, t.z, options)) for t in ctiles ]
+        #print(f'{tile=}, {base_tile_group=}')
+        create_overview_tile(z + 1, output_folder=str(tiles_dir), tile_job_info=tile_job_info, options=options, base_tiles=base_tile_group)
  
 
-
-
-
-if __name__ == '__main__':
-    import sys
-    os.umask(0o000)
-    retile_list_file = sys.argv[1]
-    tiles_dir.mkdir(parents=True, exist_ok=True)
-    tiffs_dir.mkdir(parents=True, exist_ok=True)
-    retile_sheets = Path(retile_list_file).read_text().split('\n')
-    retile_sheets = set([ r.strip() for r in retile_sheets if r.strip() != '' ])
-
-    print('reading index file')
+def get_sheet_data():
     with open(INDEX_FILE, 'r') as f:
         index_data = json.load(f)
 
-    print('preparing index to tiles maps')
     sheets_to_box = {}
     for f in index_data['features']:
         sheet_no = f['properties']['EVEREST_SH'].replace('/', '_')
         box = f['geometry']['coordinates'][0][:-1]
         sheets_to_box[sheet_no] = box
 
-    data = get_sheets_to_basetile_mapping(index_data)
-    base_tiles_to_sheets = data['base_tiles_to_sheets']
-    sheets_to_base_tiles = data['sheets_to_base_tiles']
+    return sheets_to_box
 
-    sheets_to_pull_by_sheet = {}
-    all_affected_tiles_by_sheet_no = {}
-    all_tiles_to_pull = set()
-    full_affected_tile_list = set()
+
+def get_base_tile_sheet_mappings(sheets_to_box, base_zoom):
+    sheets_to_base_tiles = {}
+    base_tiles_to_sheets = {}
+
+    for sheet_no, box in sheets_to_box.items():
+        tiles = set(mercantile.tiles(box[0][0], box[2][1], box[2][0], box[0][1], [base_zoom])) 
+        sheets_to_base_tiles[sheet_no] = tiles
+        for tile in tiles:
+            if tile not in base_tiles_to_sheets:
+                base_tiles_to_sheets[tile] = set()
+            base_tiles_to_sheets[tile].add(sheet_no)
+
+    return sheets_to_base_tiles, base_tiles_to_sheets
+
+
+def delete_unwanted_tiles(tiles_to_keep, z):
+    delete_count = 0
+    for file in tiles_dir.glob(f'{z}/*/*.webp'):
+        y = int(file.name[:-5])
+        x = int(file.parent.name)
+            
+        disk_tile = mercantile.Tile(z=z, x=x, y=y)
+        if disk_tile not in tiles_to_keep:
+            #print(f'deleting {disk_tile}')
+            file.unlink()
+            delete_count += 1
+    print(f'deleted {delete_count} files at level {z}')
+
+
+def create_vrt_file(sheets):
+    tiff_list = [ tiffs_dir.joinpath(f'{p_sheet}.tif').resolve() for p_sheet in sheets ]
+    tiff_list = [ str(f) for f in tiff_list if f.exists() ]
+
+    tiff_list_str = ' '.join(tiff_list)
+
+    vrt_file = f'{tiffs_dir}/combined.vrt'
+    run_external(f'gdalbuildvrt {vrt_file} {tiff_list_str}')
+    convert_paths_in_vrt(vrt_file)
+
+    return vrt_file
+
+
+if __name__ == '__main__':
+    import sys
+    retile_list_file = sys.argv[1]
+
+    tiles_dir.mkdir(parents=True, exist_ok=True)
+    tiffs_dir.mkdir(parents=True, exist_ok=True)
+
+    retile_sheets = Path(retile_list_file).read_text().split('\n')
+    retile_sheets = set([ r.strip() for r in retile_sheets if r.strip() != '' ])
+
+    zmin = 0
+    zmax = 14
+
+    print('getting base tiles to sheet mapping')
+    sheets_to_box = get_sheet_data()
+    sheets_to_base_tiles, base_tiles_to_sheets = get_base_tile_sheet_mappings(sheets_to_box, zmax)
+
+    all_affected_tiles = set()
+
+    print('calculating sheets to pull')
+    affected_base_tiles = set()
     for sheet_no in retile_sheets:
-        tiles_to_pull = set()
-        print(f'handling sheet - {sheet_no}')
-        affected_base_tiles = set(sheets_to_base_tiles[sheet_no])
+        affected_base_tiles.update(sheets_to_base_tiles[sheet_no])
+    all_affected_tiles.update(affected_base_tiles)
 
-        sheets_to_pull = set()
-        for btile in affected_base_tiles:
-            sheets_involved = base_tiles_to_sheets[btile]
-            sheets_to_pull.update(sheets_involved)
+    sheets_to_pull = set()
+    for tile in affected_base_tiles:
+        sheets_to_pull.update(base_tiles_to_sheets[tile])
+    print(f'{sheets_to_pull=}')
 
-        print(f'{sheets_to_pull=}')
-        sheets_to_pull_by_sheet[sheet_no] = sheets_to_pull
-                        
-        all_affected_tiles_by_zoom = {}
-        box = sheets_to_box[sheet_no]
-        all_affected_tiles = set(mercantile.tiles(box[0][0], box[2][1], box[2][0], box[0][1], range(z_min, z_max + 1))) 
-        full_affected_tile_list.update(all_affected_tiles)
-        print('affected tiles')
-        for tile in all_affected_tiles:
-            print(get_tile_file(tile))
+    print('pulling in the sheets involved')
+    copy_sheets_over(sheets_to_pull)
 
-        all_affected_tiles_by_sheet_no[sheet_no] = all_affected_tiles
-        for tile in all_affected_tiles:
-            if tile.z not in all_affected_tiles_by_zoom:
-                all_affected_tiles_by_zoom[tile.z] = set()
-            all_affected_tiles_by_zoom[tile.z].add(tile)
+    print('creating vrt file from sheets involved')
+    vrt_file = create_vrt_file(sheets_to_pull)
 
-        for z in range(z_min, z_max):
-            for tile in all_affected_tiles_by_zoom[z]:
-                for ctile in mercantile.children(tile):
-                    if ctile not in all_affected_tiles:
-                        tiles_to_pull.add(ctile)
+    print('creating tiles for base zoom with a vrt')
+    create_base_tiles(f'{vrt_file}', str(tiles_dir), f'{z_max}')
 
-        print('tiles_to_pull')
-        for tile in tiles_to_pull:
-            print(get_tile_file(tile))
-        all_tiles_to_pull.update(tiles_to_pull)
+    print('deleting unwanted base tiles')
+    delete_unwanted_tiles(affected_base_tiles, zmax)
 
-        print('pulling in sheets')
-        copy_sheets_over(sheets_to_pull)
-        print('pulling in tiles')
-        copy_tiles_over(tiles_to_pull)
-        print('deleting affected tiles')
-        deleted_count = 0
-        for tile in all_affected_tiles:
-            tile_file = Path(get_tile_file(tile))
-            if tile_file.exists():
-                deleted_count += 1
-                tile_file.unlink()
-        print(f'deleted {deleted_count} tiles')
-        # for the base level create a vrt of all the sheets needed for this sheet
-        tiff_list = [ tiffs_dir.joinpath(f'{p_sheet}.tif').resolve() for p_sheet in sheets_to_pull ]
-        #tiff_list = [ tiffs_dir.joinpath(f'{p_sheet}.tif').name for p_sheet in sheets_to_pull ]
-        tiff_list = [ str(f) for f in tiff_list if f.exists() ]
-        tiff_list_str = ' '.join(tiff_list)
-        vrt_file = f'{tiffs_dir}/{sheet_no}.vrt'
-        run_external(f'gdalbuildvrt {vrt_file} {tiff_list_str}')
-        convert_paths_in_vrt(vrt_file)
+    prev_affected_tiles = affected_base_tiles
+    for z in range(zmax-1, zmin-1, -1):
+        print(f'handling level {z}')
 
-        # and run gdal2tiles for just the base tiles
-        print('creating tiles for base zoom with a vrt')
-        create_tiles(f'{vrt_file}', str(tiles_dir), f'{z_max}')
-        # then run gdal2tiles for all the other levels with just the sheet
-        print('creating tiles for all levels with just the sheet')
-        create_upper_tiles(all_affected_tiles_by_zoom)
+        curr_affected_tiles = set()
+        for tile in prev_affected_tiles:
+            curr_affected_tiles.add(mercantile.parent(tile))
+        all_affected_tiles.update(curr_affected_tiles)
+
+        child_tiles_to_pull = set()
+        for ptile in curr_affected_tiles:
+            for ctile in mercantile.children(ptile):
+                if ctile not in prev_affected_tiles:
+                    child_tiles_to_pull.add(ctile)
+
+        print('copying additional child tiles required for curr level')
+        copy_tiles_over(child_tiles_to_pull)
+
+        print('creating tiles for current level')
+        create_upper_tiles(curr_affected_tiles)
+
+        print('removing unwanted child tiles')
+        delete_unwanted_tiles(prev_affected_tiles, z+1)
+
+        prev_affected_tiles = curr_affected_tiles
+
     if not TILES_TO_PMTILES:
         print('pushing back tiles to main')
-        push_tiles(full_affected_tile_list)
+        push_tiles(all_affected_tiles)
     #shutil.rmtree('staging')
+
+    print('All Done!!!')
 
 

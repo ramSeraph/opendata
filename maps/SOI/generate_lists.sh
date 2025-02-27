cfc87d1925f6573fbe73c40fd203266bacdbe55f
ramSeraph <kandimalla.sreeram@gmail.com>
2025-02-28 04:57:11 +0530

    SOI: more list gen fixes

--- maps/SOI/generate_lists.sh
+++ maps/SOI/generate_lists.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [[ $1 == 'pdfs' ]]; then
-  gh release view soi-pdfs --json assets -q '.assets[] | "\(.size) \(.name)"' > list.txt
+  gh release view soi-pdfs --json assets -q '.assets[] | "\(.size) \(.name)"' | grep '\.pdf' > list.txt
   gh release upload soi-pdfs --clobber list.txt
   rm list.txt
   exit 0
 fi
 
 if [[ $1 == 'tiffs' ]]; then
-  gh release view soi-tiffs --json assets -q '.assets[] | "\(.size) \(.name)"' > list.txt
+  gh release view soi-tiffs --json assets -q '.assets[] | "\(.size) \(.name)"' | grep '\.tiff' > list.txt
   gh release upload soi-tiffs --clobber list.txt
   rm list.txt
   exit 0

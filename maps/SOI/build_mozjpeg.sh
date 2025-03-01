a2332ce619c041209dab263521cf500b5863a57a
ramSeraph <kandimalla.sreeram@gmail.com>
2025-03-01 15:27:26 +0530

    SOI: add base scripts for compression

---
+++ maps/SOI/build_mozjpeg.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# for installing on mac m1 follow instruction in below link
+# https://stackoverflow.com/questions/78456062/error-while-installing-imagemin-mozjpeg-on-macos-npm-err-spawn-unknown-syste
+#
+
+npm i mozjpeg
+cp node_modules/mozjpeg/vendor/cjpeg bin/
+rm -rf node_modules

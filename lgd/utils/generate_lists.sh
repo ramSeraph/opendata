3622ddc2f14945bfe3f30932b0d87ba7e6a5fd59
ramSeraph <kandimalla.sreeram@gmail.com>
2025-03-06 18:22:30 +0530

    LGD: add url list to the releases

--- lgd/utils/generate_lists.sh
+++ lgd/utils/generate_lists.sh
@@ -2,5 +2,10 @@
 
 echo "getting file list"
 gh release view lgd-latest --json assets -q '.assets[] | "\(.size) \(.name)"' | grep '.7z$' > listing_archives.txt
+cat listing_archives.txt | cut -d" " -f2 | xargs -I {} echo "https://github.com/ramSeraph/opendata/releases/download/lgd-latest/{}" > url_list.txt
+
 echo "uploading listing"
 gh release upload lgd-latest listing_archives.txt --clobber
+gh release upload lgd-latest url_list.txt --clobber
+
+rm listing_archives.txt url_list.txt

ac81f72f9bd199a820636907253203891711e49d
ramSeraph <kandimalla.sreeram@gmail.com>
2025-03-06 17:05:25 +0530

    LGD: fix release script

--- lgd/utils/push_to_release.sh
+++ lgd/utils/push_to_release.sh
@@ -11,11 +11,11 @@ while read comp; do
   if [[ $comp == "CHANGES" ]]; then
     cd data/raw/$curr_date
     7z a changes.csv.7z changes.csv
-    gh release upload lgd-latest changes.csv.7z
+    gh release upload lgd-latest changes.csv.7z --clobber
     rm changes.csv.7z
     cd -
     cp data/raw/changes/dates_covered.txt data/raw/changes/changes_dates_covered.txt
-    gh release upload lgd-latest data/raw/changes/changes_dates_covered.txt
+    gh release upload lgd-latest data/raw/changes/changes_dates_covered.txt --clobber
     rm data/raw/changes/changes_dates_covered.txt
   else
     fname=$(cat scrape/site_map.json | jq -r --arg comp $comp '.[] | select(.comp == $comp) | .file')

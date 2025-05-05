ba6a8ba6ca35e78f9d018b5506470ae3b1a5db31
ramSeraph <kandimalla.sreeram@gmail.com>
2025-05-05 10:33:49 +0530

    change to new urls in sync report

--- docs/assets/js/lgd/wikidata/index.js
+++ docs/assets/js/lgd/wikidata/index.js
@@ -1,5 +1,5 @@
 
-const externalUrl = 'https://indianopenmaps.fly.dev/lgd/wikidata/reports';
+const externalUrl = 'reports';
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);

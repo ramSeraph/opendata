746892991208ef2e0f247e9cb8f9d153f99f101f
ramSeraph <kandimalla.sreeram@gmail.com>
2025-07-30 12:40:18 +0530

    one more typo fix

--- maps/SOI/util/download_sheets.sh
+++ maps/SOI/util/download_sheets.sh
@@ -7,6 +7,6 @@ gh release download soi-pdfs -p listing_files.csv --clobber
 cat $work_file | xargs -I {} bash -c "[[ -e data/raw/{}.pdf ]] || echo {}.pdf" > to_download.txt
 for file in $(cat to_download.txt); do
     echo "Downloading $file"
-    cat listing_files.csv | grep "^${file},$" | cut -d"," -f3 | xargs -I {} wget -P data/raw {}
+    cat listing_files.csv | grep "^${file}," | cut -d"," -f3 | xargs -I {} wget -P data/raw {}
 done
 rm to_download.txt listing_files.csv

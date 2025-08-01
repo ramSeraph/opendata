de60d5c71ab99135a443691a1df20c597378d3d4
ramSeraph <kandimalla.sreeram@gmail.com>
2025-08-01 20:56:36 +0530

    LGD: add support for archiving older months

--- lgd/utils/get_prev_month_year.py
+++ lgd/utils/get_prev_month_year.py
@@ -2,6 +2,16 @@ from datetime import datetime, timedelta, UTC
 from pathlib import Path
 import sys
 
-d = datetime.now(UTC).replace(day=1) - timedelta(days=1)
+out_file = sys.argv[1]
+if len(sys.argv) != 3:
+    months_back = 1
+else:
+    months_back = int(sys.argv[2])
+
+d = datetime.now(UTC)
+while months_back > 0:
+    d = d.replace(day=1) - timedelta(days=1)
+    months_back -= 1
+
 m = d.strftime('%b%Y')
-Path(sys.argv[1]).write_text(m)
+Path(out_file).write_text(m)

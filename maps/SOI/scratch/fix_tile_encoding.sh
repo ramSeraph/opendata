4552f5b45f85c40572be8d9e8d5c009d81c9e8ec
ramSeraph <kandimalla.sreeram@gmail.com>
2025-10-08 23:08:59 +0530

    Drop SOI processing code from this repo, website and data remain for now

--- maps/SOI/scratch/fix_tile_encoding.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-cat to_fix_objs.txt | xargs -I {} gsutil setmeta -h "Content-Type:image/webp" gs://soi_data/{}
-
--- maps/SOI/util/get_work_for_tiler.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-gh release download soi-tiffs -p listing_files.csv -O tiffs_listing.csv --clobber
-gh release download soi-latest -p listing_files.csv -O tiled_listing.csv --clobber
-
-comm -23 <(cat tiffs_listing.csv| cut -d"," -f1 | cut -d"." -f1 | sort) <(cat tiled_listing.csv | cut -d"," -f1 | cut -d"." -f1 | sort) | cut -f1 | sed '/^[[:space:]]*$/d' > $1
-
-rm tiled_listing.csv tiffs_listing.csv
